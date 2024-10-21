7b7eeb95f0ac809240c86e92165ddbfe08c72df4
mmrecoybot <contact.mustafizur@gmail.com>
2024-10-21 10:43:46 +0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
+
+export const metadata: Metadata = {
+  title: "Mst Rohany Akter Portfolio",
+  description: "Mst Rohany Akter Portfolio as name Rohany",
+  icons: {
+    icon: "vercel.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
